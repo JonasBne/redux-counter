ec830654fdbb4ece8cb447f596b61bdcf80bd677
Jonas <boone.jonas@outlook.be>
2022-03-21 11:58:59 +0100

    refactor: added counter handlers

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,21 @@
-import classes from './Counter.module.css';
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "../store/counterSlice";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      <div className={classes.value}>{count}</div>
+      <button className={classes.button} onClick={() => dispatch(increment())}>
+        Increment
+      </button>
+      <button className={classes.button} onClick={() => dispatch(decrement())}>
+        Decrement
+      </button>
     </main>
   );
 };
