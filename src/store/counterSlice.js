c131e78102496f0c69fb5d25067688c81278f927
Jonas <boone.jonas@outlook.be>
2022-03-21 11:53:58 +0100

    refactor: added counter reducer to the store

--- src/store/counterSlice.js
+++ src/store/counterSlice.js
@@ -17,4 +17,4 @@ export const counterSlice = createSlice({
 
 export const { increment, decrement } = counterSlice.actions;
 
-export default counterSlice.reducer;
+export const counterReducer = counterSlice.reducer;
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { counterReducer } from "./counterSlice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    counter: counterReducer,
+  },
 });
 
 export default store;
